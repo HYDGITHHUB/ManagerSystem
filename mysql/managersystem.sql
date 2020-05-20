6679f9d26e9f4c16fe2c9c412f230e7247944fb3
HYDGITHHUB <2607223301@qq.com>
2020-05-20 16:11:46 +0800

    build:build MD

--- mysql/managersystem.sql
+++ mysql/managersystem.sql
@@ -44,7 +44,7 @@ CREATE TABLE `research_money` (
 
 LOCK TABLES `research_money` WRITE;
 /*!40000 ALTER TABLE `research_money` DISABLE KEYS */;
-INSERT INTO `research_money` VALUES (11,'韩勇','已审核','2020-04-30 07:24:32','能量守恒','应用性','国家级','能量守恒','能量守恒',10000,NULL),(16,'韩勇','已审核','2020-05-05 10:13:39','与外天空文明的交流','经验研究性','校级','与他人沟通，了解他人的故事','沟通是解决问题的最好方式',20000,NULL);
+INSERT INTO `research_money` VALUES (63,'韩勇','已审核','2020-05-20 06:39:01','无线未来','应用性','省级','无限未来','无限未来',NULL,NULL);
 /*!40000 ALTER TABLE `research_money` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,8 +77,40 @@ CREATE TABLE `research_moneyed` (
 
 LOCK TABLES `research_moneyed` WRITE;
 /*!40000 ALTER TABLE `research_moneyed` DISABLE KEYS */;
-INSERT INTO `research_moneyed` VALUES (11,'韩勇','已审核','2020-04-30 07:24:32','能量守恒','应用性','国家级','能量守恒','能量守恒',10000,1200),(14,'韩勇','已审核','2020-04-30 07:46:24','无人汽车','应用性','省级','实现无人汽车驾驶','可以帮助人们休闲出行',10000,22000),(16,'韩勇','已审核','2020-05-05 10:13:39','与外天空文明的交流','经验研究性','校级','与他人沟通，了解他人的故事','沟通是解决问题的最好方式',10000,12000);
+INSERT INTO `research_moneyed` VALUES (57,'韩勇','已审核','2020-05-18 02:26:28','无线技术的未来','应用性','省级','未来可期','未来可期',10000,15000),(58,'h韩勇','已审核','2020-05-18 05:43:54','水化油','经验研究性','国家级','未来可期','未来可期',10000,NULL),(59,'韩勇','已审核','2020-05-18 05:44:28','5G时代','应用性','国家级','未来可期','未来可期',12000,15000),(60,'韩勇','已审核','2020-05-18 06:20:49','能量守恒','应用性','国家级','未来可期','未来可期',200000,200),(61,'张三','已审核','2020-05-18 16:56:32','虚拟数据张三','应用性','国家级','虚拟数据','虚拟数据',200000,250000),(62,'韩勇','已审核','2020-05-20 06:34:59','激光武器','经验研究性','国家级','未来可期','未来可期',100000,12000);
 /*!40000 ALTER TABLE `research_moneyed` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `research_moneying`
+--
+
+DROP TABLE IF EXISTS `research_moneying`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `research_moneying` (
+  `project_id` bigint(20) NOT NULL,
+  `project_owner` varchar(20) COLLATE utf8_bin NOT NULL,
+  `project_state` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `project_theme` varchar(20) COLLATE utf8_bin NOT NULL,
+  `project_type` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_grade` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_prospect` varchar(100) COLLATE utf8_bin NOT NULL,
+  `project_describe` varchar(200) COLLATE utf8_bin NOT NULL,
+  `project_money` bigint(20) DEFAULT NULL,
+  `project_moneyed` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `research_moneying`
+--
+
+LOCK TABLES `research_moneying` WRITE;
+/*!40000 ALTER TABLE `research_moneying` DISABLE KEYS */;
+/*!40000 ALTER TABLE `research_moneying` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -100,7 +132,7 @@ CREATE TABLE `research_project` (
   `project_describe` varchar(200) COLLATE utf8_bin NOT NULL,
   `project_money` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,7 +141,7 @@ CREATE TABLE `research_project` (
 
 LOCK TABLES `research_project` WRITE;
 /*!40000 ALTER TABLE `research_project` DISABLE KEYS */;
-INSERT INTO `research_project` VALUES (20,'韩勇','未审核','2020-05-07 06:23:47','外天空对话','应用性','国家级','友好沟通','友好沟通是进步的桥梁',NULL);
+INSERT INTO `research_project` VALUES (64,'韩勇','未审核','2020-05-20 06:40:06','虚假数据1','应用性','国家级','虚假数据1','虚假数据1',NULL);
 /*!40000 ALTER TABLE `research_project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +153,7 @@ DROP TABLE IF EXISTS `research_projected`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `research_projected` (
-  `project_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) NOT NULL,
   `project_owner` varchar(20) COLLATE utf8_bin NOT NULL,
   `project_state` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '已审核',
   `project_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -132,7 +164,7 @@ CREATE TABLE `research_projected` (
   `project_describe` varchar(200) COLLATE utf8_bin NOT NULL,
   `project_money` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +173,7 @@ CREATE TABLE `research_projected` (
 
 LOCK TABLES `research_projected` WRITE;
 /*!40000 ALTER TABLE `research_projected` DISABLE KEYS */;
-INSERT INTO `research_projected` VALUES (11,'韩勇','已审核','2020-04-30 07:24:32','能量守恒','应用性','国家级','能量守恒','能量守恒',10000),(14,'韩勇','已审核','2020-04-30 07:46:24','无人汽车','应用性','省级','实现无人汽车驾驶','可以帮助人们休闲出行',NULL),(16,'韩勇','已审核','2020-05-05 10:13:39','与外天空文明的交流','经验研究性','校级','与他人沟通，了解他人的故事','沟通是解决问题的最好方式',NULL);
+INSERT INTO `research_projected` VALUES (57,'韩勇','已审核','2020-05-18 02:26:28','无线技术的未来','应用性','省级','未来可期','未来可期',10000),(58,'h韩勇','已审核','2020-05-18 05:43:54','水化油','经验研究性','国家级','未来可期','未来可期',10000),(59,'韩勇','已审核','2020-05-18 05:44:28','5G时代','应用性','国家级','未来可期','未来可期',12000),(60,'韩勇','已审核','2020-05-18 06:20:49','能量守恒','应用性','国家级','未来可期','未来可期',200000),(61,'张三','已审核','2020-05-18 16:56:32','虚拟数据张三','应用性','国家级','虚拟数据','虚拟数据',200000),(62,'韩勇','已审核','2020-05-20 06:34:59','激光武器','经验研究性','国家级','未来可期','未来可期',100000),(63,'韩勇','已审核','2020-05-20 06:39:01','无线未来','应用性','省级','无限未来','无限未来',NULL);
 /*!40000 ALTER TABLE `research_projected` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -156,8 +188,7 @@ CREATE TABLE `research_result` (
   `project_id` bigint(20) NOT NULL,
   `project_owner` varchar(20) COLLATE utf8_bin NOT NULL,
   `project_state` varchar(10) COLLATE utf8_bin NOT NULL,
-  `project_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `project_time_end` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `project_time` date DEFAULT NULL,
   `project_result_state` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '未结题',
   `project_theme` varchar(20) COLLATE utf8_bin NOT NULL,
   `project_type` varchar(10) COLLATE utf8_bin NOT NULL,
@@ -185,7 +216,7 @@ CREATE TABLE `research_result` (
 
 LOCK TABLES `research_result` WRITE;
 /*!40000 ALTER TABLE `research_result` DISABLE KEYS */;
-INSERT INTO `research_result` VALUES (11,'韩勇','已审核','2020-04-30 07:24:32','2020-05-07 07:43:44','未结题','能量守恒','应用性','国家级','能量守恒','能量守恒',10000,1200,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `research_result` VALUES (60,'韩勇','已审核','2020-05-18','未结题','能量守恒','应用性','国家级','未来可期','未来可期',200000,200,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(62,'韩勇','已审核','2020-05-20','未结题','激光武器','经验研究性','国家级','未来可期','未来可期',100000,12000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `research_result` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -200,7 +231,7 @@ CREATE TABLE `research_resulted` (
   `project_id` bigint(20) NOT NULL,
   `project_owner` varchar(20) COLLATE utf8_bin NOT NULL,
   `project_state` varchar(10) COLLATE utf8_bin NOT NULL,
-  `project_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `project_time` date DEFAULT NULL,
   `project_time_end` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `project_result_state` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '已结题',
   `project_theme` varchar(20) COLLATE utf8_bin NOT NULL,
@@ -231,92 +262,77 @@ CREATE TABLE `research_resulted` (
 
 LOCK TABLES `research_resulted` WRITE;
 /*!40000 ALTER TABLE `research_resulted` DISABLE KEYS */;
-INSERT INTO `research_resulted` VALUES (14,'韩勇','已审核','2020-04-30 07:46:24','2020-05-07 06:16:09','已结题','无人汽车','应用性','省级','实现无人汽车驾驶','可以帮助人们休闲出行',10000,22000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'100','很好'),(16,'韩勇','已审核','2020-05-05 10:13:39','2020-05-07 07:43:06','已结题','与外天空文明的交流','经验研究性','校级','与他人沟通，了解他人的故事','沟通是解决问题的最好方式',10000,12000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `research_resulted` VALUES (57,'韩勇','已审核','2020-05-18','2020-05-18 02:28:33','未结题','无线技术的未来','应用性','省级','未来可期','未来可期',10000,15000,'学术专著','基础论著','未来确实很可期',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(59,'韩勇','已审核','2020-05-18','2020-05-20 06:49:47','未结题','5G时代','应用性','国家级','未来可期','未来可期',12000,15000,NULL,'基础论著','未来可期',NULL,NULL,NULL,NULL,NULL,NULL,'100','很好'),(62,'虚假信息李四','已审核','2020-05-19','2020-05-19 16:41:38','未结题','虚假信息李四','应用性','国家级','虚假信息李四','虚假信息李四',20000,12000,'学术专著','基础论著','未来可期',NULL,NULL,NULL,NULL,NULL,NULL,'99','很好');
 /*!40000 ALTER TABLE `research_resulted` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user_manager_personnel`
+-- Table structure for table `research_resulting`
 --
 
-DROP TABLE IF EXISTS `user_manager_personnel`;
+DROP TABLE IF EXISTS `research_resulting`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_manager_personnel` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_eno` int(10) NOT NULL,
-  `user_name` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  `user_pwd` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  `user_phone` bigint(20) DEFAULT NULL,
-  `user_mail` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `research_resulting` (
+  `project_id` bigint(20) NOT NULL,
+  `project_owner` varchar(20) COLLATE utf8_bin NOT NULL,
+  `project_state` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_time` date DEFAULT NULL,
+  `project_result_state` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '未结题',
+  `project_theme` varchar(20) COLLATE utf8_bin NOT NULL,
+  `project_type` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_grade` varchar(10) COLLATE utf8_bin NOT NULL,
+  `project_prospect` varchar(100) COLLATE utf8_bin NOT NULL,
+  `project_describe` varchar(200) COLLATE utf8_bin NOT NULL,
+  `project_money` bigint(20) DEFAULT NULL,
+  `project_moneyed` bigint(20) DEFAULT NULL,
+  `project_result_type` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `project_monograph_type` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `project_monograph_content` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  `project_press_type` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `project_press_content` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  `project_patent_type` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `project_patent_content` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  `project_technology_type` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `project_technology_content` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_manager_personnel`
+-- Dumping data for table `research_resulting`
 --
 
-LOCK TABLES `user_manager_personnel` WRITE;
-/*!40000 ALTER TABLE `user_manager_personnel` DISABLE KEYS */;
-INSERT INTO `user_manager_personnel` VALUES (1,2017110409,'韩勇','111111',17361041018,'2607223301@qq.com'),(2,2017110425,'刘海锋','222222',22222222222,'2222222222@qq.com'),(3,2017110433,'宋博宇','333333',33333333333,'3333333333@qq.com'),(4,2017110436,'唐梦予','444444',44444444444,'4444444444@qq.com'),(5,2017110456,'郑园','555555',55555555555,'5555555555@qq.com');
-/*!40000 ALTER TABLE `user_manager_personnel` ENABLE KEYS */;
+LOCK TABLES `research_resulting` WRITE;
+/*!40000 ALTER TABLE `research_resulting` DISABLE KEYS */;
+INSERT INTO `research_resulting` VALUES (61,'张三','已审核','2020-05-19','未结题','虚拟数据张三','应用性','国家级','虚拟数据','虚拟数据',200000,250000,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `research_resulting` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user_personnel`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user_personnel`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_personnel` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_eno` int(10) DEFAULT NULL,
-  `user_name` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  `user_pwd` varchar(24) COLLATE utf8_bin NOT NULL,
-  `user_phone` bigint(20) DEFAULT NULL,
-  `user_mail` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `user` (
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` int(20) DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_personnel`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `user_personnel` WRITE;
-/*!40000 ALTER TABLE `user_personnel` DISABLE KEYS */;
-INSERT INTO `user_personnel` VALUES (1,2017110409,'韩勇','123456',17361041018,'2607223301@qq.com'),(2,2017110425,'刘海锋','222222',22222222222,'22222222222@qq.com'),(3,2017110433,'宋博宇','333333',33333333333,'33333333333@qq.com'),(4,2017110448,'唐梦予','444444',44444444444,'44444444444@qq.com'),(5,2017110456,'郑园','555555',55555555555,'55555555555@qq.com'),(27,2017110401,'张三','111111',NULL,NULL),(30,2017110409,'王五','111111',NULL,NULL);
-/*!40000 ALTER TABLE `user_personnel` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_sys_manager_personnel`
---
-
-DROP TABLE IF EXISTS `user_sys_manager_personnel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_sys_manager_personnel` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_eno` int(10) DEFAULT NULL,
-  `user_name` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  `user_pwd` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  `user_phone` bigint(20) DEFAULT NULL,
-  `user_mail` varchar(24) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_sys_manager_personnel`
---
-
-LOCK TABLES `user_sys_manager_personnel` WRITE;
-/*!40000 ALTER TABLE `user_sys_manager_personnel` DISABLE KEYS */;
-INSERT INTO `user_sys_manager_personnel` VALUES (1,2017110409,'韩勇','123456',17361041018,'2607223301@qq.com');
-/*!40000 ALTER TABLE `user_sys_manager_personnel` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (10,'韩勇','111111',1),(11,'刘海锋','111111',1),(12,'李昊源','111111',3),(13,'郑园','111111',2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -328,4 +344,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-13  9:00:21
+-- Dump completed on 2020-05-20 15:43:57
